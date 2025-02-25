4994b4b5794a3b61236424c606244b8e42389569
Estifanos Girma <estifanosgirma4@gmail.com>
2025-02-25 15:27:56 +0300

    commit5

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 export default function Navbar(){
     return(
-        <div style={{position:'absolute',left:'9px',top:'15px'}}>
+        <div style={{position:'absolute',justifyContent:'center',left:'9px',top:'15px'}}>
         <nav>
-      <ul style={{listStyle:'none',display:'flex',gap:'20px'}}>
+      <ul style={{backgroundColor:'black',istStyle:'none',display:'flex',gap:'20px'}}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
