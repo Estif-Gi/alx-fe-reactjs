474b6893292be5bd211be85463a8bed7e0d5db89
Estifanos Girma <estifanosgirma4@gmail.com>
2025-02-25 12:40:55 +0300

    commit4

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,23 +7,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserProfile from './components/UserProfile'
-import ExampleComponent from './components/exp'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
        
+      <div>
       <Header />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
-        
       <WelcomeMessage />  
-      
-      </div>
       <Footer />
-      <ExampleComponent/>
+      </div>
+    
     </>
   )
 }
