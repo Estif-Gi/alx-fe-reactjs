d344b1e6e33e8205ba0999c85cb39a675029f446
Estifanos Girma <estifanosgirma4@gmail.com>
2025-02-28 21:05:57 +0300

    update

--- recipe-sharing-app/src/components/RecipeStore.js
+++ recipe-sharing-app/src/components/RecipeStore.js
@@ -4,7 +4,7 @@ const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  setRecipes: (recipes) => set({recipes}),
 }));
 
 export default useRecipeStore;
