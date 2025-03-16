de6ac1cab5571935cbf6a69d3aad3b1408bc0de4
Estifanos Girma <estifanosgirma4@gmail.com>
2025-03-16 22:15:19 +0300

    tailwind

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
      "./public/index.html",
   ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
     extend: {},
