08ff0bd7c477923d37ef2ab9c2e701c5b3269aac
Estifanos Girma <estifanosgirma4@gmail.com>
2025-03-19 16:36:34 +0300

    ingredient and steps

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useState , useEffect } from "react";
+import { Link } from "react-router-dom";
 import data from "../data.json"
 function HomePage() {
     const[recipes, setRecipes] = useState([]);
@@ -8,8 +9,9 @@ useEffect(()=>{
     return(
         <div className=" grid grid-cols-2 grid-rows-2">
             {recipes.map(item =>(
-            
-                <div className=" transition ease-in duration-200 m-6 bg-gray-200 hover:bg-gray-100 shadow-md rounded-md  w-auto ml-4 mt-3 p-2 " key={item.id}>
+            <Link to={`/recipe/${item.id}`}>
+
+                <div kye={item.id} className=" transition ease-in duration-200 m-6 bg-gray-200 hover:bg-gray-100 shadow-md rounded-md  w-auto ml-4 mt-3 p-2 " key={item.id}>
                     <div className=" grid md:grid-cols-3 sm:flex">
                      <img className=" w-44 h-40 rounded-lg hover:shadow-2xl lg:w-72 lg:h-72 md:rounded-3xl transform hover:scale-95 transition ease-in duration-100" src={item.image} alt={item.title} />
                     <div className="grid grid-cols-1">
@@ -18,6 +20,7 @@ useEffect(()=>{
                     </div>
                      </div>
                 </div>
+            </Link>
 
             ))}
         </div>
