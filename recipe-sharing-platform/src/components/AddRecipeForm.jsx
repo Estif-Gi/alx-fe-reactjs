351c0a8e185472f7a5c0ca1bd4bc598fedbf17bf
Estifanos Girma <estifanosgirma4@gmail.com>
2025-03-20 12:03:04 +0300

    add recipe is added

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -6,19 +6,20 @@ const validationSchema = Yup.object({
     title:Yup.string().required("Food Name is required"),
     summary:Yup.string().required("Add your thoughts about the food"),
     ingredients:Yup.string().required('How to mack the food'),
-    instructions:Yup.string().required('At minimum the food should contain 2 instructions')
+    instructions:Yup.string().required('At minimum the food should contain 2 steps')
 })
 
 
 const  AddRecipeForm = () => {
     const [submit , setSubmit] = useState(false);
     const [items , setItems ] = useState([]);
+    const [input, setInput] = useState("");
     
-    // function handelSubmit() {
-    //     setItems(Input.split(',').map(item=>item.trim()))
+    function handelSubmit() {
+        setItems(Input.split(',').map(item=>item.trim()))
 
 
-    // }
+    }
     return ( 
         <div className="relative ">
             <Formik className="" initialValues={{
@@ -45,7 +46,7 @@ const  AddRecipeForm = () => {
                         <Field type="text" placeholder="Ingredients" name="ingredients" className="border-2 border-gray-400 rounded-lg pl-3 pt-3 m-4 pb-24 w-72 shadow-2xl" required/> 
                         <ErrorMessage name="ingredients" component="p"/>
                        
-                        <Field type="text" placeholder="Instructions" name="instructions" className="border-2 border-gray-400 rounded-lg pl-3 pt-3 m-4 pb-24 w-72 shadow-2xl" required/> 
+                        <Field type="text" onChange={(e)=>setInput(e.target.value)} onSubmit={handelSubmit}name="instructions" className="border-2 border-gray-400 rounded-lg pl-3 pt-3 m-4 pb-24 w-72 shadow-2xl" required/> 
                         <ErrorMessage name="instructions" component="p"/>
 
                         <button className="absolute bottom-40 left-80 border-red-500 border-2 rounded-xl p-2 hover:text-white hover:bg-red-500" type="submit">Add Recipe</button>
